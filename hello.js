ed37c828314c2a2de0f941862d9b795b7a21e2bf
abhishek372 <abhishekkumar111109@gmail.com>
2022-12-05 03:10:18 +0530

    statements added hello.js

--- hello.js
+++ hello.js
@@ -1,1 +1,8 @@
 console.log("Hello World")
+
+// Works in console in the browser
+console.log("%cHello", "color:red, font-size:40px")   //gives Red color of size 40px for word Hello
+
+// join two or more strings in one line
+console.log("Hello" + " World")
+console.log("Hello", "there", "World")
