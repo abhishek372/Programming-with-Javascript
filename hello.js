ca2bf639898de6e024c69b72e6e473c74ec9725b
abhishek372 <abhishekkumar111109@gmail.com>
2022-12-06 01:07:04 +0530

    Number guessing game

--- hello.js
+++ hello.js
@@ -6,3 +6,8 @@ console.log("%cHello", "color:red, font-size:40px")   //gives Red color of size 
 // join two or more strings in one line
 console.log("Hello" + " World")
 console.log("Hello", "there", "World")
+
+// Function to print in the document 
+document.write("Hello World")
+
+// 
