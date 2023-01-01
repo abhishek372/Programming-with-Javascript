d0c2b90ec4d0e9bf0eb9c00bf6166d7d9ac30b39
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-01 22:59:48 +0530

    Division operator

---
+++ operators/division.js
@@ -0,0 +1,9 @@
+// Division operator (/) gives the quotient of its operands where left operand is dividend and right operand is th divisor.
+// During division, the string at divisor or dividend end are treated as number.
+
+console.log(4/2);
+console.log(7/2);
+console.log(10/4.0);
+console.log(15/"2");
+console.log("6"/"2");
+console.log(5/0);
