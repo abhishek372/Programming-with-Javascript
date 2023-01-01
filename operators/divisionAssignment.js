ba2b5c7340b6471da03d0aaaa685ecd97bae186c
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-02 00:00:12 +0530

    Division assignment

---
+++ operators/divisionAssignment.js
@@ -0,0 +1,11 @@
+// Divison assignment (/=) operator gives the quotient of the operands and then store the result to operand
+
+let a = 24;
+a /= 3;
+console.log(a);
+
+a /= '4';
+console.log(a);
+
+a /= 'hello';
+console.log(a);
