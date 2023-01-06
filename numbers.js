76d24ced85896fee0161b9ad22636fbad6e34343
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-06 23:52:21 +0530

    Operators folder updated

--- numbers.js
+++ numbers.js
@@ -1,7 +1,7 @@
 // Numbers in js 
 // It can be integer or decimal point numbers 
 
-console.log(10**2)
+console.log(10**2)   
 console.log(22%5) 
 console.log((5*4)+2-1)
 console.log(20/(4/2)+5*2)
--- strings.js
+++ strings.js
@@ -1,7 +1,7 @@
 // Strings
 // Collection of characters enclosed within single or double quotes.
 
-var a = "hello world!"
+var a = "hello world!";
 console.log(a)
 
 console.log("It is too cold in the house")
