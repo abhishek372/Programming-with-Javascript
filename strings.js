1f7073c6676931ff839f9a21f64324e40fe6d4ff
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-08 00:00:34 +0530

    String concept added

--- strings.js
+++ strings.js
@@ -1,5 +1,6 @@
 // Strings
 // Collection of characters enclosed within single or double quotes.
+// Also characters inside back tick quotes are called as strings
 
 var a = "hello world!";
 console.log(a)
@@ -8,3 +9,4 @@ console.log("It is too cold in the house")
 console.log('where are you going?')
 console.log("It's raining outside.")
 console.log("let's do coding.") 
+console.log(`Its coding time.`) 
