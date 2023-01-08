7cd83e5314cb826a505633c96407f56d44fa3d3b
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-08 16:22:42 +0530

    Dynamic and weak typing

---
+++ Dynamic_weak_typing/demo.js
@@ -0,0 +1,15 @@
+// Javascript is a dynamic as well as weak typing programming language.
+
+// Dynamic typing
+let a = 50;         // a is of number data type
+
+console.log(a);
+
+a = "Hello";         // a is of string data type
+console.log(a);
+
+// Weak typing
+let b = 5;          // b is of number data type
+b += '5';           // b is now of string data type
+
+console.log(b)
