cf807a45a6d00dadaf08d43f45fc6a8b30bfa5ec
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-12 21:52:12 +0530

    Count object using loop

---
+++ Loops/loops.js
@@ -0,0 +1,10 @@
+// Loops is  the way to perform some tasks repeatedly under some conditions
+// like playing game until we lose it. So, the loosing is the condition
+
+// Various types of looops is there in JS
+// for statement
+// while statement
+// do..while statement
+// for..in 
+// for..of 
+
