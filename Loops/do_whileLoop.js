69b9efc4f662921d96901e8e4a69d7bb3617f9e6
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-14 00:20:22 +0530

    do while loop executed

---
+++ Loops/do_whileLoop.js
@@ -0,0 +1,10 @@
+// do..while loop statement executes until the specified condition evaluates to false.
+
+
+// loop iterates at least once and reiterates until i is no longer less than 5.
+
+let i = 0;
+do {
+    i +=1;
+    console.log(i);
+}while(i < 5);
