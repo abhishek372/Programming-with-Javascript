eb52447526886b71eae629f9f16af6f01c8c33db
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-16 02:17:29 +0530

    deleted some files

--- StringMethods/substr.js
+++
@@ -1,7 +0,0 @@
-// Using substr() method, we can get substring from the given string
-// It takes two arguments, the starting index and number of characters to slice.
-
-let string = "Javascript";
-console.log(string.substr());   //by default, substr() method takes starting index as 0 and number of characters as String length
-
-console.log(string.substr(2, 5));
