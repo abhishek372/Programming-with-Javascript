424fc5f0b98807bbe34540d1a43f19136727d7f8
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-15 22:05:09 +0530

    Changes in loops file

--- Loops/loops.js
+++ Loops/loops.js
@@ -1,9 +1,12 @@
 // Loops is  the way to perform some tasks repeatedly under some conditions
 // like playing game until we lose it. So, the loosing is the condition
 
-// Various types of looops is there in JS
+// Various types of loops is there in JS
 // for statement
 // while statement
 // do..while statement
 // for..in 
 // for..of 
+// forEach method which runs the callback function for each object
+
+
