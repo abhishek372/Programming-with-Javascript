c744e1130285b6499f684aebc09197fa6ef31370
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-19 02:28:03 +0530

    Numeric separator file added

---
+++ Number_numberTypes/numericSeparator.js
@@ -0,0 +1,11 @@
+// Numeric separator is used to make the numeric literals more readable
+
+const num = 1_00_000;
+console.log(num);
+
+// Specially, more used for large numbers
+const billion = 1_000_000_000;
+console.log(billion);
+
+
+// It can be used for all number types.
