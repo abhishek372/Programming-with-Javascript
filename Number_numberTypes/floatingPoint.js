53dd8893eb873e819e713710b39029ad99712e3e
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-19 02:02:30 +0530

    floating point numbers

---
+++ Number_numberTypes/floatingPoint.js
@@ -0,0 +1,19 @@
+// Floating point numbers are decimal point number with atleast one digit after point
+
+let number = 0.56;
+console.log({number});
+
+let newNum = 0.99;
+let sum = number + newNum;
+console.log(sum);
+
+
+// Floating point numbers are accurate upto 17 decimal points
+let n1 = 0.2 + 0.1;
+console.log(n1);            // 0.30000000000000004
+
+
+let num = 5e4;              // 5*10^4
+console.log({num})          // 50000
+
+
