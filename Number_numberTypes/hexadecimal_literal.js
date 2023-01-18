4cdbb99954b9f3e3fe3e7195950094694c0e5990
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-19 01:53:25 +0530

    hexadecimal literal

---
+++ Number_numberTypes/hexadecimal_literal.js
@@ -0,0 +1,8 @@
+// hexadecimal numbers start with 0x or 0X followed by any number of hexadecimal digits
+// digits between (0 to 9) are allowed otherwise treated as decimal
+
+let num = 0x45;
+console.log(num);
+
+let number = 0x881;
+console.log(number);
