84a2efe53ed87cff11c891708ac1315f4ddee6a1
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-24 20:08:04 +0530

    Message boxes

---
+++ Primitive_referenceVariables.js
@@ -0,0 +1,8 @@
+// Javascript engine allocates two types of values to the variables 
+// Primitive value
+// Reference value
+
+// When we declare variables, JS engines allocates memory on two different locations
+
+// Stack memory 
+// Heap memory
---
+++ popupMessage.js
@@ -0,0 +1,6 @@
+// JS provide global methods to display popup message boxes 
+
+alert("hello, this is a alert message box")
+
+let num = 10;
+alert(`The number is : ${num}`);
