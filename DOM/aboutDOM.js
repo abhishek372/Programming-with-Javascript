95b1601e39d190e7e05db25cfd85223cd244740e
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-26 01:06:57 +0530

    DOM folder created

---
+++ DOM/aboutDOM.js
@@ -0,0 +1,8 @@
+/*
+    DOOM (Document Object Model) is an API for manipulating HTML and XML documents
+    DOM represents HTML and XML documents as a tree of nodes. It provides functions way to 
+    add, remove and modify the part of HTML documents.
+
+    DOM is a cross-platform and language-independent way of manipulating HTML and XML documents.
+*/
+
