aead19ff36516c114f2793b42991641def9fe22e
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-29 23:58:18 +0530

    Changes in Nested loops

--- Loops/nested_loops.js
+++ Loops/nested_loops.js
@@ -2,6 +2,14 @@
 
 for(var i=1; i<=10; i++){
     for(var j=1; j<=10; j++){
-        console.log(`${i} * ${j} =`, i*j );
+        console.log(`${i} X ${j} =`, i*j );
+    }
+}
+
+// Divison table
+
+for(var i=100; i>0; i=i-10){
+    for(var j = 10; j>0; j=j-5){
+        console.log(`${i} / ${j} =`, i/j);
     }
 }
