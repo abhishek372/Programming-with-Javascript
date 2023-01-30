2fade89f3054d2e4769ce0c41979d1019c1cf682
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-30 23:40:32 +0530

    DOM file updated

--- DOM/aboutDOM.js
+++ DOM/aboutDOM.js
@@ -2,7 +2,27 @@
     DOOM (Document Object Model) is an API for manipulating HTML and XML documents
     DOM represents HTML and XML documents as a tree of nodes. It provides functions way to 
     add, remove and modify the part of HTML documents.
-
     DOM is a cross-platform and language-independent way of manipulating HTML and XML documents.
+
+    <html>
+    <head>
+        <title>JavaScript DOM</title>
+    </head>
+    <body>
+        <p>Hello DOM!</p>
+    </body>
+    </html>
+
+    DOM converts the above HTML template into tree of nodes.
+
+    In this document is the root node.The root node has one child node which is the <html> element. 
+    The <html> element is called the document element.
+    In an HTML document, the document element is the <html> element. 
+    Each markup can be represented by a node in the tree.
+*/
+
+
+/* 
+    We can also get the type of node by .nodeType property
 */
 
