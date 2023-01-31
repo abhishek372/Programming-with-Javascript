4c9d77e3a09a250f3eeeaa76a58c93ce82c564af
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 19:49:07 +0530

    Ceil method

---
+++ Objects/ceil.js
@@ -0,0 +1,8 @@
+
+// Ceil method
+// Rounds up the decimal to the nearest integer
+console.log(Math.ceil(0.0001));
+console.log(Math.ceil(0.04));
+console.log(Math.ceil(1.34));
+console.log(Math.ceil(-1.34));
+console.log(Math.ceil(2.99));
