d9559a9b30effda89ee84a0b609a117da97e04ce
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 19:15:05 +0530

    Math object methods

---
+++ Objects/mathObject.js
@@ -0,0 +1,27 @@
+// Javascript has Math object which have some handy number constants
+
+// Number constants
+console.log(Math.PI);
+console.log(Math.E);
+console.log(Math.LN10);
+console.log(Math.LN2);
+console.log(Math.LOG10E);
+console.log(Math.LOG2E);
+console.log(Math.SQRT1_2);
+console.log(Math.SQRT2);
+
+
+// Rounding methods
+console.log(Math.round(3.45));          // rounds nearest integer based on decimal value
+console.log(Math.ceil(3.45));           // rounds up to the closest integer
+console.log(Math.floor(3.45));          // rounds down to the closest integer
+console.log(Math.trunc(4.567));         // remove the decimal part
+
+
+// Arithmetic and calculus methods
+console.log(Math.pow(2,3));                     // calculates the number 2 to the power of 3
+console.log(Math.abs(-12.45));                  // absolute value of -12.45
+console.log(Math.cbrt(64));                     // cube root of 64
+console.log(Math.sqrt(64));                     // square root of 64
+console.log(Math.min(56, 23, 12, 34, 45));      // minimum value of all inputs
+console.log(Math.max(56, 23, 12, 34, 45));      // maximum value of all inputs
