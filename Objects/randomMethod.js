1880db066260b097dacb8761af1d8ab04e02cca5
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 19:32:41 +0530

    Random Method

---
+++ Objects/randomMethod.js
@@ -0,0 +1,10 @@
+// random method - built-in method comes under Math object
+// generates random number between 0 and 0.99
+
+Math.random();
+
+decimal = Math.random(10)
+console.log(decimal)
+
+// generate random number between 0 and 10
+console.log(decimal*10);
