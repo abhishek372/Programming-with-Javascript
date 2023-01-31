90206c424a13365edc04ce9dd5a636f195666fef
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 21:10:25 +0530

    changes in file

--- StringMethods/string_array.js
+++ StringMethods/string_array.js
@@ -8,4 +8,8 @@ let arr = [1, 3, 5, 6, 7];
 // console.log(typeof(arr + 'hello'));      // string
 
 // console.log(s.concat('hello')==arr.concat('hello'));
-console.log(s.pop());               // Error
+// console.log(s.pop());               // Error
+
+// const ar = new Array(5);
+// ar[0] = 'he'
+// console.log(ar.length);
