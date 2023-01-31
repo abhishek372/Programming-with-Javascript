332cd9eb1ee5f3762a8f7ca14a325316e8e146c4
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 18:40:48 +0530

    Objects folder created

---
+++ Objects/objects.js
@@ -0,0 +1,11 @@
+// Objects is a collection of items in key-value pairs
+// It can be created using object literal notation
+
+var user = {};          //create an Empty object
+console.log(user);
+
+// declaring new properties using dot notation
+user.name = 'John';
+user.email = 'john12@gmail.com';
+user.phone = '349823';
+console.log(user);
