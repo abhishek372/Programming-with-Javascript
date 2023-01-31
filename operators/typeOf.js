9c125019635802b2bd5e2b965a9e02e3acadbfb9
abhishek372 <abhishekkumar111109@gmail.com>
2023-01-31 22:13:58 +0530

    typeof method

---
+++ operators/typeOf.js
@@ -0,0 +1,16 @@
+// typeOf method checks the type of value
+
+console.log(typeof(12));
+
+console.log(typeof('Hello'));
+
+console.log(typeof(True));
+
+console.log(typeof(False));
+
+console.log(typeof(5>2));
+
+console.log(typeof([2, 3, 5, 6]));
+
+console.log(typeof({1:'apple', 2:'orange', 3:'banana'}));
+
