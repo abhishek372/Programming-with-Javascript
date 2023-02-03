e256e3f66d8f4d95a1601f18a9175abdc8977efa
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-03 20:42:47 +0530

    Functional Programming started

---
+++ Functional Programming/functionalProgramming.js
@@ -0,0 +1,6 @@
+// There are two common programming paradigms used in programming paradigms
+
+// Functional programming
+// Object Oriented Programming
+
+// Functional Programming - It is the way to implement data and functions work independently
