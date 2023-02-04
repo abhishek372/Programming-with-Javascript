d3529ea12670dabbbbefc4f98e18352874a09cf3
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-05 01:01:08 +0530

    Changes in Higher order functions

--- Functional Programming/higherOrderFunctions.js
+++ Functional Programming/higherOrderFunctions.js
@@ -3,7 +3,23 @@
 // It accepts other functions as arguments
 // It returns functions when invoked
 
+function getNumber(){
+    return 30;
+}
+
+function randomNum() {
+    return Math.floor((Math.random() * 10) + 1);
+}
+
+function specificNum() { 
+    return 42 
+}
+
 function addTwoNums(getNumber1, getNumber2) {
     console.log(getNumber1() + getNumber2());
 }
 
+addTwoNums(getNumber, getNumber)
+addTwoNums(specificNum, randomNum)        
+addTwoNums(randomNum, randomNum)
+addTwoNums(specificNum,specificNum)
