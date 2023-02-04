b56f8ae134c7eea1b542963152468d8ecf7e1fe6
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-04 20:48:16 +0530

    Functional Programming

--- Functional Programming/functionalProgramming.js
+++ Functional Programming/functionalProgramming.js
@@ -3,4 +3,17 @@
 // Functional programming
 // Object Oriented Programming
 
-// Functional Programming - It is the way to implement data and functions work independently
+// Functional Programming - It is the way to implement data and functions work independently and
+// the functions use the data to perform some operations and then return some data
+
+let oneNumber = 5;
+let otherNumber = 8;
+
+function add(a, b) {
+    return a + b;
+}
+
+console.log(add(oneNumber, otherNumber));
+
+// Note: here both the functions and data are working independently and function 'add' working taking
+// two parameters to return the sum of the number.
