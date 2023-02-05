129d3ca8829c9b763e5257c9151c49a26a856bb0
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-05 16:50:23 +0530

    .\varKeyword.js

---
+++ var_let_const/varKeyword.js
@@ -0,0 +1,15 @@
+// var keyword
+
+// We can access a var variable before initalization.
+var user;
+console.log(user);
+
+
+// We can declare and redeclare same variable without any errors
+
+var user = 'Mary';
+var user = 'John';
+var user = 'Maraon';
+
+console.log(user);
+
