208211e7a47c655633ae2520bc0e627f9fdb0a70
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-05 16:53:38 +0530

    let keyword

---
+++ var_let_const/letKeyword.js
@@ -0,0 +1,17 @@
+// let keyword
+
+// We cannot access a let variable before we declare it
+// console.log('user');    // Error
+let user;
+
+// We can declare a unassigned variable with let
+// user = 'Anna';
+// console.log('user');
+
+// We can't redeclare a declared variable
+// let user = 'Mary';          //  Syntax Error
+console.log(user);
+
+// But we can reasssign a declared variable
+user = 'Harry';
+console.log(user);
