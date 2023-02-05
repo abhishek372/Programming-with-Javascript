03c11d209ed52d9dc4b8ab62b104362dcade05c1
abhishek372 <abhishekkumar111109@gmail.com>
2023-02-05 17:18:52 +0530

    Const variable

---
+++ var_let_const/constKeyword.js
@@ -0,0 +1,14 @@
+// const keyword
+
+// The const variable must be intialized before
+// console.log(user);          // Error
+// const user;
+
+// We can't access the const variable before initalization
+// console.log(user);
+const user = 'John';
+
+// We can also don't redeclare the const variable
+user = 'John';
+console.log(user);
+
